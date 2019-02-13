87bdeb6475d3652ef42be1abf6fad7fbd2c330e9
GianlucaTardio <Gianluca.Tardio@gmail.com>
2019-02-13 12:45:54 +0100

    init

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+
+  if (!function_exists('import_style'))
+  {
+    function import_style()
+    {
+      wp_enqueue_style('boolean', get_template_directory_uri() . '/style.css');
+    }
+
+    add_action('wp_enqueue_scripts', 'import_style');
+
+  }
+
+
+ ?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+
+  <h1>Ciao Classe 3</h1>
+
+<?php get_footer(); ?>
