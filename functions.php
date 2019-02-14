e411ffb30353d5bda2a6df99dabc636a71faec0e
GianlucaTardio <Gianluca.Tardio@gmail.com>
2019-02-14 09:24:13 +0100

    Bolean Theme

--- functions.php
+++ functions.php
@@ -11,5 +11,26 @@
 
   }
 
+  if (!function_exists('boolean_setup'))
+  {
+    function boolean_setup()
+    {
+      register_nav_menus(array(
+        'header' => 'header'
+      ));
+    }
+    add_action('after_setup_theme', 'boolean_setup');
+  }
 
+  if (!function_exists('boolean_widgets_init'))
+  {
+    function boolean_widgets_init()
+    {
+      register_sidebar(array(
+        'name'            => 'boolean-sidebar',
+	       'id'             => 'boolean-sidebar'
+      ));
+    }
+    add_action('widgets_init', 'boolean_widgets_init');
+  }
  ?>
